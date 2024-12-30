d0c21dbd9a5455ef5339bc5d3fe8b9fdf4a7ecee
sasha <sasha.park.designer@gmail.com>
2024-12-30 14:37:33 +0900

    스탑워치 구현

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ function App() {
   return (
     <>
     <Clock/>
+    <StopWatch/>
     <TodoInput setTodo={setTodo}/>
     <TodoList todo={todo} setTodo={setTodo}/>
 
@@ -38,6 +39,46 @@ const Clock = () => {
     <div>
       <h1>{time.toLocaleTimeString()}</h1>
     </div>
+  )
+}
+
+
+const formatTime = (seconds) => {
+  const timeString =  `${String(Math.floor(seconds / 3600)).padStart(2, "0")}:${String(Math.floor((seconds % 3600) / 60)).padStart(2, "0")}:${String(seconds % 60).padStart(2, "0")}`;
+  return timeString;
+}
+
+
+const StopWatch = () => { 
+
+  const [time, setTime] = useState(0);
+  const [isOn, setIsOn] = useState(false);
+  const timerRef = useRef(null);
+
+  useEffect(() => {
+    if (isOn === true) {
+     const timerId = setInterval(() => {
+        setTime((prev) => prev + 1)
+      },1000);
+      timerRef.current = timerId;
+    } else {
+      clearInterval(timerRef.current);
+    }
+    
+  }, [isOn]);
+
+
+  return (
+    <>
+    {formatTime(time)}
+    <button onClick={()=>setIsOn((prev) => !prev)}>{isOn ? "끄기":"켜기"}</button>
+    <button onClick={() => {
+      setTime(0);
+      setIsOn(false);
+    }
+
+    }>초기화</button>
+    </>
   )
 }
 
